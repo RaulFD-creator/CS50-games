b72214f3d61a536c5117225b2cfb1873ce08c3c1
RaulFD-creator <raulfdz9@gmail.com>
2021-05-10 00:48:15 +0200

    Weaponization of the pots

--- zelda/src/Projectile.lua
+++ zelda/src/Projectile.lua
@@ -23,6 +23,7 @@ function Projectile:init(def, x, y)
     self.state = self.defaultState
     self.states = def.states
     
+    
     -- dimensions
     self.x = x
     self.y = y
@@ -39,6 +40,7 @@ function Projectile:init(def, x, y)
     self.counter = 0 
     self.counter2 = 0
     self.counter3 = 0
+    self.thrown_speed = 2
 end
     
 function Projectile:update(dt)
@@ -50,17 +52,17 @@ function Projectile:update(dt)
         self.x = self.room.player.x 
         self.y = self.room.player.y - 12
     elseif self.direction_thrown == 'right' and self.counter < 4 *TILE_SIZE + 1 and not self.crashed then
-        self.x = self.x + 1
-        self.counter = self.counter + 1
+        self.x = self.x + self.thrown_speed
+        self.counter = self.counter + self.thrown_speed
     elseif self.direction_thrown == 'left' and self.counter < 4 *TILE_SIZE + 1 and not self.crashed then
-        self.x = self.x - 1
-        self.counter = self.counter + 1
+        self.x = self.x - self.thrown_speed
+        self.counter = self.counter + self.thrown_speed
     elseif self.direction_thrown == 'up' and self.counter < 4 *TILE_SIZE + 1 and not self.crashed then
-        self.y = self.y - 1
-        self.counter = self.counter + 1
+        self.y = self.y - self.thrown_speed
+        self.counter = self.counter + self.thrown_speed
     elseif self.direction_thrown == 'down' and self.counter < 4 *TILE_SIZE + 1 and not self.crashed then
-        self.y = self.y + 1
-        self.counter = self.counter + 1 
+        self.y = self.y + self.thrown_speed
+        self.counter = self.counter + self.thrown_speed
     else
         self.crashed = true
     end
@@ -91,6 +93,33 @@ function Projectile:update(dt)
         end
     end
 
+    if self.thrown and not self.crashed then
+        hitboxWidth = 11
+        hitboxHeight = 11
+        if self.direction_thrown == 'left' then
+            hitboxX = self.x - hitboxWidth
+            hitboxY = self.y + 2
+        elseif self.direction_thrown == 'right' then
+            hitboxX = self.x + self.width
+            hitboxY = self.y + 2
+        elseif self.direction_thrown == 'up' then
+            hitboxX = self.x
+            hitboxY = self.y - hitboxHeight
+        else
+            hitboxX = self.x
+            hitboxY = self.y + self.height
+        end
+
+        self.potHitbox = Hitbox(hitboxX, hitboxY, hitboxWidth, hitboxHeight)
+
+        for k, entity in pairs(self.room.entities) do
+            if entity:collides(self.potHitbox) then
+                entity:damage(1)
+                gSounds['hit-enemy']:play()
+            end
+        end    
+    end
+
     if love.keyboard.isDown('return') and self.counter2 > 1 then
         self:throw(self.room.player.direction)   
     end
@@ -98,9 +127,10 @@ end
 
 function Projectile:throw(direction)
     self.thrown = true
-    self.direction_thrown = direction
     self.room.player.carrying = false
+    self.direction_thrown = direction
     self.room.player:changeState('idle')
+    gSounds['sword']:play()
 end
 
 function Projectile:render(adjacentOffsetX, adjacentOffsetY)

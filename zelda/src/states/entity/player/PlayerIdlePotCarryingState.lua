e666df56e64bb60edf4889b2b55dd4497b617113
RaulFD-creator <raulfdz9@gmail.com>
2021-05-09 21:09:53 +0200

    More progress towards lifting pot

--- zelda/src/states/entity/player/PlayerIdlePotCarryingState.lua
+++ zelda/src/states/entity/player/PlayerIdlePotCarryingState.lua
@@ -3,6 +3,7 @@ PlayerIdlePotCarryingState = Class{__includes = BaseState}
 function PlayerIdlePotCarryingState:init(player, dungeon)
     self.player = player
     self.dungeon = dungeon
+    self.player:changeAnimation('lift-' .. self.player.direction)
 end
 
 function PlayerIdlePotCarryingState:enter(params)
@@ -10,6 +11,7 @@ function PlayerIdlePotCarryingState:enter(params)
     -- render offset for spaced character sprite (negated in render function of state)
     self.player.offsetY = 5
     self.player.offsetX = 0
+    self.player.currentAnimation:refresh()
 end
 
 function PlayerIdlePotCarryingState:update(dt)
@@ -22,4 +24,15 @@ function PlayerIdlePotCarryingState:update(dt)
         --self.entity:changeState('swing-sword')
         gSounds['pickup']:play()
     end
+
+    if self.player.currentAnimation.timesPlayed > 0 then
+        self.player.currentAnimation.timesPlayed = 0
+        self.player:changeState('carry-pot-idle')
+    end
+end
+
+function PlayerIdlePotCarryingState:render()
+    local anim = self.player.currentAnimation
+    love.graphics.draw(gTextures[anim.texture], gFrames[anim.texture][anim:getCurrentFrame()],
+        math.floor(self.player.x - self.player.offsetX), math.floor(self.player.y - self.player.offsetY))
 end

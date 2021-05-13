2547d58f5619269fef4ff660ab48386747c98f52
RaulFD-creator <raulfdz9@gmail.com>
2021-05-13 22:06:10 +0200

    Gem Spawning

--- helicopter-master/Assets/Resources/Scripts/HeliController.cs
+++ helicopter-master/Assets/Resources/Scripts/HeliController.cs
@@ -60,6 +60,15 @@ public class HeliController : MonoBehaviour {
 		GetComponents<AudioSource>()[0].Play();
 		GetComponent<ParticleSystem>().Play();
 	}
+	public void PickupGem()
+	{
+		coinTotal += 5;
+
+		// trigger audio playback and emit particles from particle system
+		GetComponents<AudioSource>()[0].Play();
+		GetComponent<ParticleSystem>().Play();
+	}
+
 
 	public void Explode() {
 		explosionSound.Play();

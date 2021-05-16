abdbad18abd27e6f6d425182f32e192af380ee99
RaulFD-creator <raulfdz9@gmail.com>
2021-05-16 21:37:06 +0200

    Defining initial functions

---
+++ desert_quest/src/Dependencies.lua
@@ -0,0 +1,11 @@
+-- Calling main libraries
+
+Class = require 'lib/class'
+Event = require 'lib/knife.event'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
+
+-- Loading main functions
+require 'src/Util'
+require 'src/Constants'
+

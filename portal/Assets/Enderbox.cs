a116129565a39934fea6b34242fe887fda5af481
RaulFD-creator <raulfdz9@gmail.com>
2021-05-16 21:16:09 +0200

    Final commitment

---
+++ portal/Assets/Enderbox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Enderbox : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(0, 2f, 0, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ portal/Assets/GameOverText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverText : MonoBehaviour
+{
+
+	public GameObject Enderbox;
+	private Text text;
+
+	void Start()
+	{
+		text = GetComponent<Text>();
+		text.color = new Color(0, 0, 0, 0);
+	}
+	void Update()
+	{
+		if (Enderbox == null)
+		{
+			text.color = new Color(0, 0, 0, 1);
+			text.text = "You won!";
+		}
+	}
+}

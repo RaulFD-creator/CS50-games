85c04bb57a2a8830034fdd22f3228d8143df7742
RaulFD-creator <raulfdz9@gmail.com>
2021-05-17 15:54:26 +0200

    Camera movement

--- desert_quest/src/states/PlayState.lua
+++ desert_quest/src/states/PlayState.lua
@@ -5,18 +5,30 @@ function PlayState:enter(params)
 end
 
 function PlayState:init()
-    gSounds['musics']['play']:stop()
+    gSounds['musics']['title_screen']:stop()
     gSounds['musics']['play']:play()
+    self.backgroundX = 0
+    self.camX = 0
+    self.camY = 0
 end
 
 function PlayState:update(dt) 
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
+    if love.keyboard.wasPressed('right') or love.keyboard.wasPressed('d') then
+        self.camX = self.camX + 10
+    elseif love.keyboard.wasPressed('left') or love.keyboard.wasPressed('a') then
+        self.camX = self.camX - 10
+    end
+    self.backgroundX = (self.camX / 3) % 256
 end
 
 function PlayState:render()
-    love.graphics.draw(gTextures['backgrounds']['2'], 0, 0, 0, 
-        VIRTUAL_WIDTH / gTextures['backgrounds']['2']:getWidth(),
-        VIRTUAL_HEIGHT / gTextures['backgrounds']['2']:getHeight())
+    love.graphics.push()
+    love.graphics.draw(gTextures['backgrounds']['2'], gFrames['background'][1], math.floor(-self.backgroundX), 0)
+    love.graphics.draw(gTextures['backgrounds']['2'], gFrames['background'][1], math.floor(-self.backgroundX),
+        gTextures['backgrounds']['2']:getHeight(), 0, 1, 1)
+    love.graphics.translate(-math.floor(self.camX), -math.floor(self.camY))
+    love.graphics.pop()
 end
